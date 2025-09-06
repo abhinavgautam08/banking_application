d5d42951b3b8402b1434c2462119d0eba8d710c4
Abhinav Adarsh <abhinava8mar@gmail.com>
2025-09-06 19:28:05 +0530

    Constructor

--- Main.java
+++ Main.java
@@ -8,4 +8,13 @@ class Account {
     private double balance;
     private String email;
     private String phoneNumber;
-   }
+
+    public Account(int accountNumber, String accountHolderName, double initialDeposit,
+                   String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialDeposit;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
