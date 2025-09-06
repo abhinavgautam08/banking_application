f727b895615e62f55643f77019fec83bbee9b04a
Abhinav Adarsh <abhinava8mar@gmail.com>
2025-09-06 19:31:04 +0530

    Withdraw money from account

--- Main.java
+++ Main.java
@@ -28,4 +28,14 @@ class Account {
             System.out.println("Deposit amount must be positive.");
         }
     }
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdrawal amount must be positive.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful! Current balance: " + balance);
+        }
+    }
 }
