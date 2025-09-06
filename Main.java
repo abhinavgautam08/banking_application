3656645d51836f2126051d4e4bf3e79db7124789
Abhinav Adarsh <abhinava8mar@gmail.com>
2025-09-06 19:26:23 +0530

    Account Class

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+
+class Account {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+    private String email;
+    private String phoneNumber;
+   }
